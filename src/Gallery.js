e08403c0793410568cf227b638ab3073f6e34803
Sonja Wilmsen <s_wilmsen@hotmail.com>
2016-07-17 18:03:12 +0200

    added spinner

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,56 @@
 import React from 'react';
 
+function imagesLoaded(parentNode) {
+  const imgElements = parentNode.querySelectorAll('img');
+  for (const img of imgElements) {
+    if (!img.complete) {
+      return false;
+    }
+  }
+  return true;
+}
+
 class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  handleImageChange() {
+    // In React 0.13 use: 'this.refs.gallery.getDOMNode()'
+    const galleryElement = this.refs.gallery;
+    this.setState({
+      loading: !imagesLoaded(galleryElement),
+    });
+  }
+
+  renderSpinner() {
+    if (!this.state.loading) {
+      return null;
+    }
+    return (
+      <span className="spinner" />
+    );
+  }
+
   renderImage(imageUrl) {
-    return(
+    return (
       <div>
-        <img src={imageUrl} />
+        <img
+          src={imageUrl}
+          onLoad={this.handleImageChange.bind(this)}
+          onError={this.handleImageChange.bind(this)}
+          />
       </div>
     );
   }
 
   render() {
     return (
-      <div className="gallery">
+      <div className="gallery" ref="gallery">
+        {this.renderSpinner()}
         <div className="images">
           {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
         </div>
@@ -19,9 +58,7 @@ class Gallery extends React.Component {
     );
   }
 }
-
 Gallery.propTypes = {
   imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
 };
-
 export default Gallery;
